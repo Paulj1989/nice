2d943036dc061e756c6d456db3cd09da5e3b4488
paulj1989 <paul@paulrjohnson.net>
2022-02-03 21:28:26 +0000

    Add check function

---
+++ R/check.R
@@ -0,0 +1,13 @@
+#' Checking whether your R output is really nice
+#'
+#' @keywords nice
+#' @export
+#' @examples
+#' check(69)
+
+check <- function(x) {
+  if (x %in% c(69, 0.69))
+  {
+    print("Nice!")
+  }
+}
