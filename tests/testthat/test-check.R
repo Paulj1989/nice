c02d795a3e0c51f9b9cbd6844299f0990d2802bc
paulj1989 <paul@paulrjohnson.net>
2022-02-19 12:57:30 +0000

    Add tests for correct positive and negative responses

--- tests/testthat/test-check.R
+++ tests/testthat/test-check.R
@@ -1,4 +1,14 @@
-test_that("check returns correct response", {
+test_that("check returns correct type of response", {
   nice <- check(69)
   expect_type(nice, "character")
+})
+
+test_that("check returns correct positive response", {
+  nice <- check(69)
+  expect_equal(nice, "Nice!")
+})
+
+test_that("check returns correct negative response", {
+  nice <- check(420)
+  expect_equal(nice, "Not very nice.")
 })
--- tests/testthat/test-check_df.R
+++ tests/testthat/test-check_df.R
@@ -1,5 +1,18 @@
-test_that("check_df returns correct response", {
+test_that("check_df returns correct type of response", {
   df <- data.frame(1:100)
   nice <- check_df(df)
   expect_type(nice, "character")
+})
+
+
+test_that("check_df returns correct positive response", {
+  df <- data.frame(1:100)
+  nice <- check_df(df)
+  expect_equal(nice, "Nice!")
+})
+
+test_that("check_df returns correct negative response", {
+  df <- data.frame(1:10)
+  nice <- check_df(df)
+  expect_equal(nice, "Not very nice.")
 })
