a7aaa98f183d1d95be368a3edd720cba287b262a
Paul Johnson <paul@paulrjohnson.net>
2023-05-07 00:04:52 +0100

    Clean up package functions, docs, and testing

--- R/check.R
+++ R/check.R
@@ -1,12 +1,23 @@
-#' Checking whether your R output is really nice
+#' Checking whether an R output is really nice
+#'
+#' @param x An R object. Can be a list or vector, and values can be of type
+#' numeric, double, or character.
 #'
 #' @keywords nice
+#'
 #' @export
+#'
 #' @examples
-#' check(69)
-
+#' x <- 23 * 3
+#' check(x)
+#'
+#' x <- c(1, 2, 3, 4, 5)
+#' check(x)
+#'
+#' x <- c("ten", "twenty", "thirty", "sixty nine")
+#' check(x)
 check <- function(x) {
-  if (x %in% c(69, 0.69)) {
+  if (any(x %in% c(69, 0.69, 0.069, 0.0069, "Sixty Nine", "sixty nine"))) {
     print("Nice!")
   } else {
     print("Not very nice.")
--- tests/testthat/test-check_df.R
+++ tests/testthat/test-check_df.R
@@ -1,18 +1,18 @@
-test_that("check_df returns correct type of response", {
-  df <- data.frame(1:100)
-  nice <- check_df(df)
-  expect_type(nice, "character")
+test_that("check_df function returns correct type of response", {
+  df <- data.frame(var = 1:100)
+  expect_type(check_df(df), "character")
 })
 
-
-test_that("check_df returns correct positive response", {
-  df <- data.frame(1:100)
-  nice <- check_df(df)
-  expect_equal(nice, "Nice!")
+test_that("check_df function returns correct positive responses", {
+  num_df <- data.frame(var = 1:100)
+  char_df <- data.frame(var = c("ten", "twenty", "thirty", "sixty nine"))
+  expect_equal(check_df(num_df), "Nice!")
+  expect_equal(check_df(char_df), "Nice!")
 })
 
-test_that("check_df returns correct negative response", {
-  df <- data.frame(1:10)
-  nice <- check_df(df)
-  expect_equal(nice, "Not very nice.")
+test_that("check_df returns correct negative responses", {
+  num_df <- data.frame(var = 1:10)
+  char_df <- data.frame(var = c("ten", "twenty", "thirty"))
+  expect_equal(check_df(num_df), "Not very nice.")
+  expect_equal(check_df(char_df), "Not very nice.")
 })
